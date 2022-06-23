65b9c36c0e36a7e4a917938fa6522db6402532bb
MAXHO <mahankov@sgu.ru>
2022-06-23 22:32:45 +0400

    log4j

--- src/main/java/com/example/registrationdemo/controller/LoginController.java
+++ src/main/java/com/example/registrationdemo/controller/LoginController.java
@@ -10,12 +10,14 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 @WebServlet(name = "login", value = "/login")
 public class LoginController extends HttpServlet {
-
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
     private UserService service;
     private UserMapper mapper;
 
@@ -23,22 +25,25 @@ public class LoginController extends HttpServlet {
     public void init() {
         service = new UserService();
         mapper = new UserMapper();
+        logger.trace("{} init.", this.getClass());
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String path = "/users";
         User user = mapper.toUser((UserLoginDto) request.getAttribute("user"));
-
+        logger.info("Try to get users for user {}", user);
         HttpSession session = request.getSession();
         boolean isRegistered = service.isRegistered(user);
         session.setAttribute("isAuth", isRegistered);
 
         if(!isRegistered){
+            logger.warn("User {} is not registered", user);
             request.setAttribute("color", "red");
             request.setAttribute("message", "User not registered!");
             path = "index.jsp";
         }
+        logger.debug("Move to {} url for user {}", path, user);
         request.getRequestDispatcher(path).forward(request, response);
     }
 
@@ -46,5 +51,6 @@ public class LoginController extends HttpServlet {
     public void destroy() {
         service = null;
         mapper = null;
+        logger.trace("{} destroy.", this.getClass());
     }
 }
